5e3f9285d9f41bb1f65ecd4a18ed6ee5632116b8
Sudhandar Balakrishnan <sudhandar@gmail.com>
2022-07-19 11:39:37 -0400

    tweets cleaning

--- generate_new_datasets/ieee_tweets/ieee_dataset.py
+++ generate_new_datasets/ieee_tweets/ieee_dataset.py
@@ -3,12 +3,47 @@ import pyarrow.dataset as ds
 from datasets import Dataset, DatasetDict
 import pandas as pd
 import collections
+import re
 
+mentions = re.compile(r"^@\S+|\s@\S+")
+urls = re.compile(r"https?://\S+")
+
+def remove_emojis(data):
+    emoj = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002500-\U00002BEF"  # chinese char
+        u"\U00002702-\U000027B0"
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642" 
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"  # dingbats
+        u"\u3030"
+                      "]+", re.UNICODE)
+    return re.sub(emoj, ' ', data)
+
+def process_text(text):
+    text = re.sub(r'http\S+', ' ', text)
+    text = mentions.sub(' ', text)
+    return text.strip().lower()
 
 df = pd.read_csv('ieee_tweets_labelled.csv', delimiter = ';')
 df = df.dropna()
 df = df[['text','sentiment']].drop_duplicates()
 df.columns = ['sentence','label']
+df['sentence'] = df.sentence.apply(process_text)
+df['sentence'] = df.sentence.apply(remove_emojis)
+df['sentence'] = df['sentence'].str.replace('\n',' ')
+df['sentence'] = df['sentence'].str.replace('rt',' ')
 df['sentence'] = df['sentence'].str.strip()
 df['label'] = df['label'].str.strip()
 df = df.drop_duplicates()
