ee1c68b06866d6e8908765d033c09ea6feb7de23
Sudhandar Balakrishnan <sudhandar@gmail.com>
2022-07-21 13:46:46 -0400

    utils error

--- tasks/utils.py
+++ tasks/utils.py
@@ -1,25 +1,18 @@
 from tasks.glue.dataset import task_to_keys as glue_tasks
-from tasks.superglue.dataset import task_to_keys as superglue_tasks
 
 GLUE_DATASETS = list(glue_tasks.keys())
 
 
-TASKS = ["glue", "superglue", "ner", "srl", "qa"]
+TASKS = ["glue"]
 
-DATASETS = GLUE_DATASETS + SUPERGLUE_DATASETS + NER_DATASETS + SRL_DATASETS + QA_DATASETS
+DATASETS = GLUE_DATASETS
 
 ADD_PREFIX_SPACE = {
     'bert': False,
     'roberta': True,
-    'deberta': True,
-    'gpt2': True,
-    'deberta-v2': True,
 }
 
 USE_FAST = {
     'bert': True,
     'roberta': True,
-    'deberta': True,
-    'gpt2': True,
-    'deberta-v2': False,
 }
