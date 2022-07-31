e029c11da1273dde5e78ec38f87e0aeb6dab1772
Sudhandar Balakrishnan <sudhandar@gmail.com>
2022-07-31 15:40:40 -0400

    qwerty error fixed

--- generate_new_datasets/financial_phrasebank/financial_phrasebank_dataset.py
+++ generate_new_datasets/financial_phrasebank/financial_phrasebank_dataset.py
@@ -56,8 +56,30 @@ class Corruption:
         self.df = df
     
 
-    def antonym_generator(self, percentage):
-        aug = naw.AntonymAug( aug_p = 0.5 , aug_min = 10, stopwords= ['a','is','an','the','be','of','and'], stopwords_regex= '[0-9]')
+    def antonym_generator(self, percentage, word_percentage):
+        if word_percentage == 'default':
+            aug = naw.AntonymAug( aug_p = 0.5 , aug_min = 10, stopwords= ['a','is','an','the','be','of','and'], stopwords_regex= '[0-9]')
+        # elif word_percentage == 0.2:
+        #     aug = naw.AntonymAug(
+        #         aug_p = 0.1,
+        #         aug_min = 3,
+        #         stopwords = ['a','is','an','the','be','of','and','to'],
+        #         stopwords_regex = '[0-9]')
+
+        # elif word_percentage == 0.5:
+        #     aug = naw.AntonymAug(
+        #         aug_p = 0.25,
+        #         aug_min = 6,
+        #         stopwords = ['a','is','an','the','be','of','and','to'],
+        #         stopwords_regex = '[0-9]')
+
+        elif word_percentage == 1:
+            aug = naw.AntonymAug(
+                  aug_p = 1,
+                  aug_min = 10,
+                  aug_max = 20,
+                  stopwords_regex = '[0-9]')
+
         self.df['new_sentence'] = self.df['sentence'].apply(lambda x:aug.augment(x)[0])
         no_antonyms = self.df[self.df['sentence']==self.df['new_sentence']]
         can_be_converted = self.df[self.df['sentence']!=self.df['new_sentence']]
@@ -127,8 +149,30 @@ class Corruption:
         print('Final Dataframe shape:', self.df.shape)
         return self.df
 
-    def random_word_deletion(self, percentage):
-        aug = naw.RandomWordAug(action='delete', aug_p = 0.6 , aug_min = 5, stopwords= ['a','is','an','the','be','of','and'], stopwords_regex= '[0-9]')
+    def random_word_deletion(self, percentage, word_percentage):
+        if word_percentage == 'default':
+            aug = naw.RandomWordAug(action='delete', aug_p = 0.6 , aug_min = 5, stopwords= ['a','is','an','the','be','of','and'], stopwords_regex= '[0-9]')
+        elif word_percentage == 0.2:
+            aug = naw.RandomWordAug(action = 'delete',
+                aug_p = 0.1,
+                aug_min = 3,
+                stopwords = ['a','is','an','the','be','of','and','will','up','to'],
+                stopwords_regex = '[0-9]')
+
+        elif word_percentage == 0.5:
+            aug = naw.RandomWordAug(action = 'delete',
+                aug_p = 0.25,
+                aug_min = 6,
+                stopwords = ['a','is','an','the','be','of','and','will','up','to'],
+                stopwords_regex = '[0-9]')
+
+        elif word_percentage == 0.8:
+            aug = naw.RandomWordAug(action = 'delete',
+                  aug_p = 0.8,
+                  aug_min = 10,
+                  aug_max = 20,
+                  stopwords_regex = '[0-9]')
+            
         print('Dataframe shape:',self.df.shape)
         samples_to_convert = int(self.df.shape[0] * percentage)
         print('Expected conversions:', samples_to_convert)
@@ -149,33 +193,33 @@ class Corruption:
         if word_percentage == 'default':
             aug = nac.KeyboardAug( aug_char_min = 5, aug_word_min = 5, aug_word_p = 0.5, aug_char_p = 0.5 )
         elif word_percentage == 0.2:
-            aug = nac.KeyboardAug(aug_char_min = param_list_20['aug_char_min'],
-                aug_word_min = param_list_20['aug_word_min'],
-                aug_word_p = param_list_20['aug_word_p'],
-                aug_char_p = param_list_20['aug_char_p'],
-                aug_word_max = param_list_20['aug_word_max'],
-                aug_char_max = param_list_20['aug_char_max'],
-                stopwords = param_list_20['stopwords'],
-                stopwords_regex = param_list_20['stopwords_regex'])
+            aug = nac.KeyboardAug(aug_char_min = 1,
+                aug_word_min = 1,
+                aug_word_p = 0.1,
+                aug_char_p = 0.1,
+                aug_word_max = 10,
+                aug_char_max = 10,
+                stopwords = ['a','is','an','the','be','of','and','will','up','to'],
+                stopwords_regex = '[0-9]')
 
         elif word_percentage == 0.5:
-            aug = nac.KeyboardAug( aug_char_min = param_list_50['aug_char_min'],
-                aug_word_min = param_list_50['aug_word_min'],
-                aug_word_p = param_list_50['aug_word_p'],
-                aug_char_p = param_list_50['aug_char_p'],
-                aug_word_max = param_list_50['aug_word_max'],
-                aug_char_max = param_list_50['aug_char_max'],
-                stopwords = param_list_50['stopwords'],
-                stopwords_regex = param_list_50['stopwords_regex'])
+            aug = nac.KeyboardAug( aug_char_min = 2,
+                aug_word_min = 2,
+                aug_word_p = 0.25,
+                aug_char_p = 0.25,
+                aug_word_max = 8,
+                aug_char_max = 8,
+                stopwords = ['a','is','an','the','be','of','and','will','up','to'],
+                stopwords_regex = '[0-9]')
 
         elif word_percentage == 1:
-            aug = nac.KeyboardAug(aug_char_min = param_list_100['aug_char_min'],
-                aug_word_min = param_list_100['aug_word_min'],
-                aug_word_p = param_list_100['aug_word_p'],
-                aug_char_p = param_list_100['aug_char_p'],
-                aug_word_max = param_list_100['aug_word_max'],
-                aug_char_max = param_list_100['aug_char_max'],
-                stopwords_regex = param_list_100['stopwords_regex'])
+            aug = nac.KeyboardAug(aug_char_min = 5,
+                aug_word_min = 5,
+                aug_word_p = 0.5,
+                aug_char_p = 0.5,
+                aug_word_max = 40,
+                aug_char_max = 40,
+                stopwords_regex = '[0-9]')
 
         print('Dataframe shape:',self.df.shape)
         samples_to_convert = int(self.df.shape[0] * percentage)
@@ -237,7 +281,6 @@ class Corruption:
         converted.columns = ['sentence','label']
         self.df = self.df.append(converted)
         print('Final Dataframe shape:', self.df.shape)
-        self.df = self.df.sample(frac = 1, random_state = 8)
         
         return self.df
 
@@ -280,7 +323,7 @@ print(collections.Counter(train_test_valid_dataset['validation']['label']))
 train_df = train_test_valid_dataset['train'].to_pandas()
 train_df = train_df[['sentence','label']].drop_duplicates()
 corrupter = Corruption(train_df)
-corrupt_train_df = corrupter.random_character_insertion(1, word_percentage = 1)
+corrupt_train_df = corrupter.antonym_generator(1, word_percentage = 1)
 corrupt_train_dataset = Dataset(pa.Table.from_pandas(corrupt_train_df))
 corrupt_train_dataset = corrupt_train_dataset.class_encode_column("label")
 
@@ -288,7 +331,7 @@ corrupt_train_dataset = corrupt_train_dataset.class_encode_column("label")
 valid_df = train_test_valid_dataset['test'].to_pandas()
 valid_df = valid_df[['sentence','label']].drop_duplicates()
 corrupter = Corruption(valid_df)
-corrupt_valid_df = corrupter.random_character_insertion(1, word_percentage = 1)
+corrupt_valid_df = corrupter.antonym_generator(1, word_percentage = 1)
 
 corrupt_valid_dataset = Dataset(pa.Table.from_pandas(corrupt_valid_df))
 corrupt_valid_dataset = corrupt_valid_dataset.class_encode_column("label")
@@ -298,17 +341,16 @@ train_test_valid_corrupt = DatasetDict({
     'test': test_valid['test'],
     'validation': corrupt_valid_dataset})
 
-train_test_valid_corrupt.save_to_disk("./corrupt_data/random_character_insertion/financial_phrasebank_corrupt_100.hf")
+train_test_valid_corrupt.save_to_disk("./corrupt_data/antonyms/financial_phrasebank_corrupt_80.hf")
 
 
 test_df = train_test_valid_dataset['validation'].to_pandas()
 test_df = test_df[['sentence','label']].drop_duplicates()
 
-corrupt_train_df.to_csv('./corrupt_data/random_character_insertion/combined_corrupt_train_100.csv',index=False)
-corrupt_valid_df.to_csv('./corrupt_data/random_character_insertion/combined_corrupt_dev_100.csv',index=False)
+corrupt_train_df.to_csv('./corrupt_data/antonyms/combined_corrupt_train_80.csv',index=False)
+corrupt_valid_df.to_csv('./corrupt_data/antonyms/combined_corrupt_dev_80.csv',index=False)
 
 # train_df.to_csv('combined_train.csv',index=False)
 # valid_df.to_csv('combined_dev.csv',index=False)
-test_df.to_csv('./corrupt_data/random_character_insertion/combined_test.csv',index=False)
-
+test_df.to_csv('./corrupt_data/antonyms/combined_test.csv',index=False)
 
